a422d0e7cb08132d82c0ac282f3ce35fe99c16fa
Matthew Kirkendall <kirk.viola@gmail.com>
2023-03-14 22:33:08 -0500

    Lazy load first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { 
-    path: '/dashboard', 
+    path: 'dashboard', 
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   }
 ];
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { RouterModule } from '@angular/router';
 
-
+const routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class DashboardModule { }
