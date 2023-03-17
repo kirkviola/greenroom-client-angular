b28de962d60faa8d1a737ca79ee8c87a32f3a968
Matthew Kirkendall <kirk.viola@gmail.com>
2023-03-16 22:25:22 -0500

    Shared initialized, more landing page structure

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,17 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule } from '@angular/router';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 
 const routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'student',
+    component: StudentDashboardComponent
+  },
+  {
+    path: 'teacher',
+    component: TeacherDashboardComponent
   }
 ]
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    StudentDashboardComponent,
+    TeacherDashboardComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/shared/app-constants.ts
@@ -0,0 +1,1 @@
+export const baseUrl = 'http://localhost:5235/api/';
